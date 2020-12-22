85b0cffd5fd645e7a07c6ea7750e15f6d894708c
IVAN <ivanbatur5g@gmail.com>
2020-12-22 21:02:20 +0100

    Initial commit

---
+++ models/item.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ItemSchema = new Schema(
+  {
+    name: {type: String, required: true, maxlength: 100},
+    description: {type: String, required: true, maxlength: 100},
+    category: {type: Schema.Types.ObjectId, ref: "Category", required: true},
+    price: {type: Number, required: true},
+    number_in_stock: {type: Number, required: true}
+  }
+);
+
+// Virtual for author's full name
+ItemSchema
+.virtual('url')
+.get(function () {
+  return "/item/" + this._id;
+});
+
+
+//Export model
+module.exports = mongoose.model('Item', ItemSchema);
