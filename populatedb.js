bc914b39d9e7da774e6e85af1831764a7abfa32f
IVAN <ivanbatur5g@gmail.com>
2020-12-23 11:46:38 +0100

    Added controllers and populated db

--- populatedb.js
+++ populatedb.js
@@ -28,7 +28,7 @@ var items = []
 
 
 function categoryCreate(name,description, cb) {
-  var category = new category({ name: name, description: description });
+  var category = new Category({ name: name, description: description });
        
   category.save(function (err) {
     if (err) {
@@ -47,9 +47,9 @@ function itemCreate(name, description, category, price, number_in_stock, cb) {
     description: description,
     price: price,
     number_in_stock: number_in_stock,
-    category: category,
-  }    
-  var item = new item(itemdetail);    
+  } 
+  if (category != false) itemdetail.category = category;   
+  var item = new Item(itemdetail);    
   item.save(function (err) {
     if (err) {
       cb(err, null)
@@ -62,82 +62,54 @@ function itemCreate(name, description, category, price, number_in_stock, cb) {
 }
 
 
-// function bookInstanceCreate(book, imprint, due_back, status, cb) {
-//   bookinstancedetail = { 
-//     book: book,
-//     imprint: imprint
-//   }    
-//   if (due_back != false) bookinstancedetail.due_back = due_back
-//   if (status != false) bookinstancedetail.status = status
-    
-//   var bookinstance = new BookInstance(bookinstancedetail);    
-//   bookinstance.save(function (err) {
-//     if (err) {
-//       console.log('ERROR CREATING BookInstance: ' + bookinstance);
-//       cb(err, null)
-//       return
-//     }
-//     console.log('New BookInstance: ' + bookinstance);
-//     bookinstances.push(bookinstance)
-//     cb(null, book)
-//   }  );
-// }
 
 
-function createGenreAuthors(cb) {
+
+function createCategories(cb) {
     async.series([
         function(callback) {
-          authorCreate('Patrick', 'Rothfuss', '1973-06-06', false, callback);
+          categoryCreate('Vehicles', 'Cars, trucks, motorcycles..', callback);
         },
         function(callback) {
-          authorCreate('Ben', 'Bova', '1932-11-8', false, callback);
+          categoryCreate('Tools', 'Hammers, screwdrivers, nails...', callback);
         },
         function(callback) {
-          authorCreate('Isaac', 'Asimov', '1920-01-02', '1992-04-06', callback);
+          categoryCreate('Appliances', 'Washing machines, kitchen appliances...', callback);
         },
         function(callback) {
-          authorCreate('Bob', 'Billings', false, false, callback);
+          categoryCreate('Books', 'Novels, textbooks, poetry...',callback);
         },
         function(callback) {
-          authorCreate('Jim', 'Jones', '1971-12-16', false, callback);
-        },
-        function(callback) {
-          genreCreate("Fantasy", callback);
-        },
-        function(callback) {
-          genreCreate("Science Fiction", callback);
-        },
-        function(callback) {
-          genreCreate("French Poetry", callback);
-        },
+          categoryCreate('Game consoles', 'PC, Nintendo, PS4, PS5...', callback);
+        }
         ],
         // optional callback
         cb);
 }
 
 
-function createBooks(cb) {
+function createItems(cb) {
     async.parallel([
         function(callback) {
-          bookCreate('The Name of the Wind (The Kingkiller Chronicle, #1)', 'I have stolen princesses back from sleeping barrow kings. I burned down the town of Trebon. I have spent the night with Felurian and left with both my sanity and my life. I was expelled from the University at a younger age than most people are allowed in. I tread paths by moonlight that others fear to speak of during day. I have talked to Gods, loved women, and written songs that make the minstrels weep.', '9781473211896', authors[0], [genres[0],], callback);
+          itemCreate('BMW', '1 SERIES, 120KW 2008', categories[0], 4000, 1, callback);
         },
         function(callback) {
-          bookCreate("The Wise Man's Fear (The Kingkiller Chronicle, #2)", 'Picking up the tale of Kvothe Kingkiller once again, we follow him into exile, into political intrigue, courtship, adventure, love and magic... and further along the path that has turned Kvothe, the mightiest magician of his age, a legend in his own time, into Kote, the unassuming pub landlord.', '9788401352836', authors[0], [genres[0],], callback);
+          itemCreate("Mercedes", 'C Class, 140KW 2005', categories[0], 3500, 3, callback);
         },
         function(callback) {
-          bookCreate("The Slow Regard of Silent Things (Kingkiller Chronicle)", 'Deep below the University, there is a dark place. Few people know of it: a broken web of ancient passageways and abandoned rooms. A young woman lives there, tucked among the sprawling tunnels of the Underthing, snug in the heart of this forgotten place.', '9780756411336', authors[0], [genres[0],], callback);
+          itemCreate("PS5", 'Sony Play Station 5',  categories[4], 500, 10, callback);
         },
         function(callback) {
-          bookCreate("Apes and Angels", "Humankind headed out to the stars not for conquest, nor exploration, nor even for curiosity. Humans went to the stars in a desperate crusade to save intelligent life wherever they found it. A wave of death is spreading through the Milky Way galaxy, an expanding sphere of lethal gamma ...", '9780765379528', authors[1], [genres[1],], callback);
+          itemCreate("Apes and Angels", "Humankind headed out to the stars not for conquest....", categories[3], 10, 33, callback);
         },
         function(callback) {
-          bookCreate("Death Wave","In Ben Bova's previous novel New Earth, Jordan Kell led the first human mission beyond the solar system. They discovered the ruins of an ancient alien civilization. But one alien AI survived, and it revealed to Jordan Kell that an explosion in the black hole at the heart of the Milky Way galaxy has created a wave of deadly radiation, expanding out from the core toward Earth. Unless the human race acts to save itself, all life on Earth will be wiped out...", '9780765379504', authors[1], [genres[1],], callback);
+          itemCreate("Death Wave","In Ben Bova's previous novel New Earth, Jordan Kell led...", categories[3], 8, 5, callback);
         },
         function(callback) {
-          bookCreate('Test Book 1', 'Summary of test book 1', 'ISBN111111', authors[4], [genres[0],genres[1]], callback);
+            itemCreate("Washing Machine", 'Samsung 5kW',  categories[2], 590, 4, callback);
         },
         function(callback) {
-          bookCreate('Test Book 2', 'Summary of test book 2', 'ISBN222222', authors[4], false, callback)
+            itemCreate("Hammer", 'Wooden handle',  categories[1], 10, 11, callback);
         }
         ],
         // optional callback
@@ -149,8 +121,8 @@ function createBooks(cb) {
 
 
 async.series([
-    createGenreAuthors,
-    createBooks,
+    createCategories,
+    createItems,
 ],
 // Optional callback
 function(err, results) {
---
+++ routes/catalog.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// Require controller modules.
+const category_controller = require('../controllers/categoryController');
+const item_controller = require('../controllers/itemController');
+
+
+
+
+module.exports = router;
