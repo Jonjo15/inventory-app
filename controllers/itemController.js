0dbc31325bd53fd6f34a88d737d4e364e6cca60f
IVAN <ivanbatur5g@gmail.com>
2020-12-24 11:25:25 +0100

    Added form for creating item

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,5 +1,6 @@
 const Item = require("../models/item")
 const Category = require("../models/category")
+const { body,validationResult } = require('express-validator');
 
 var async = require('async');
 // Display list of all Authors.
@@ -53,14 +54,73 @@ exports.item_detail = function(req, res, next) {
 };
 
 // Display item create form on GET.
-exports.item_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: item create GET');
+exports.item_create_get = function(req, res, next) {
+
+    // Get all authors and genres, which we can use for adding to our item.
+    async.parallel({
+        items: function(callback) {
+            Item.find(callback);
+        },
+        categories: function(callback) {
+            Category.find(callback);
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        res.render('item_form', { title: 'Create Item', items: results.items, categories: results.categories });
+    });
+
 };
 
 // Handle item create on POST.
-exports.item_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: item create POST');
-};
+exports.item_create_post = [
+
+    // Validate and sanitise fields.
+    body('name', 'Name must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('description', 'Description must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('price', 'Price must not be empty or zero.').trim().isLength({ min: 1 }).escape(),
+    body('number_in_stock', 'number_in_stock must not be empty or zero').trim().isLength({ min: 1 }).escape(),
+    body('category', 'Category must not be empty.').trim().isLength({ min: 1 }).escape(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a item object with escaped and trimmed data.
+        var item = new Item(
+          { name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            number_in_stock: req.body.number_in_stock,
+            category: req.body.category
+           });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/error messages.
+
+            // Get all authors and categories for form.
+            async.parallel({
+                categories: function(callback) {
+                    Category.find(callback);
+                },
+            }, function(err, results) {
+                if (err) { return next(err); }
+
+                res.render('item_form', { title: 'Create Item', categories:results.categories, item: item, errors: errors.array() });
+            });
+            return;
+        }
+        else {
+            // Data from form is valid. Save item.
+            item.save(function (err) {
+                if (err) { return next(err); }
+                   //successful - redirect to new item record.
+                   res.redirect(item.url);
+                });
+        }
+    }
+];
 
 // Display item delete form on GET.
 exports.item_delete_get = function(req, res) {
